d4106dd7b31b441896c84648c273d4648711a391
rkj <romainknezevic@gmail.com>
2018-01-13 12:37:02 +0100

    add build.sh

---
+++ build_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+version=$(cat version.go |grep Version |sed 's,.\+ "\([[:alnum:].]\+\)",\1,')
+dst="bin"
+
+mkdir -p $dst
+
+for os in "linux" "darwin" "windows"; do
+  for arch in "amd64" "386" "arm"; do
+    case "$arch" in
+    "amd64")
+      post=""
+      ;;
+    "386")
+      post="-x86"
+      ;;
+    "arm")
+      [ "$os" != "linux" ] && continue
+      post="-arm"
+      ;;
+    esac
+
+    bin=$dst/bitbot_${version}_$os$post
+    [ "$os" = "windows" ] && bin=$bin.exe
+
+
+    echo "building $bin" >&2
+    GOARCH=$arch GOOS=$os go build -o $bin
+  done
+done
